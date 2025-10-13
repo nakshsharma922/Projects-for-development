d67d19686274946e7863048b0fb123b5e67ab9f5
nakshsharma922 <nakshsharma922@gmail.com>
2025-10-13 23:22:50 +0530

    Auto update

--- Blackjack_minigame/first.js
+++ Blackjack_minigame/first.js
@@ -1,3 +1,20 @@
-let card1;
-let card2;
+let card1 = 10;
+let card2 = 5;
+let hasBlackJack = false;
+let isAlive = true;
+let sum = card1 + card2;
 
+let message = "";
+
+function startgame() {
+    if(sum <= 20) {
+        console.log("Want to draw another card?");  
+    }
+    else if(sum === 21) {
+        console.log("Yohoo!!,you've got a blackjack");
+        isAlive = false;
+    }
+    else {
+        console.log("You're out of the game");
+    }
+}
