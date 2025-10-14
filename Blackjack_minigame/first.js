cf0f354511647c514185c646e1d7ed150795e37d
nakshsharma922 <nakshsharma922@gmail.com>
2025-10-14 16:54:23 +0530

    Auto update

--- Blackjack_minigame/first.js
+++ Blackjack_minigame/first.js
@@ -4,11 +4,10 @@ Math.floor(Math.random() * (max-min)) + min;
 let card1 = 0;
 let card2 = 0;
 let count = 0;
-
+let has_started = false;
 let hasBlackJack = false;
 let isAlive = true;
 let sum = card1 + card2;
-
 let message = "";
 
 document.getElementById("card1").innerText += card1;
@@ -16,35 +15,43 @@ document.getElementById("card2").innerText += card2;
 
 document.getElementById("sum").textContent += sum;
 
+function reset() {
+    location.reload();
+}
+
 function start() {
-    card1 = Math.floor(Math.random() * (max-min)) + min;
-    card2 = Math.floor(Math.random() * (max-min)) + min;
+    if(count===0 && !has_started) {
+        has_started = true;
+        card1 = Math.floor(Math.random() * (max-min)) + min;
+        card2 = Math.floor(Math.random() * (max-min)) + min;
 
-    sum = card1 + card2;
+        sum = card1 + card2;
 
-    document.getElementById("card1").innerText = card1;
-    document.getElementById("card2").innerText = card2;
-    document.getElementById("sum").textContent = sum;
+        document.getElementById("card1").innerText = card1;
+        document.getElementById("card2").innerText = card2;
+        document.getElementById("sum").textContent = sum;
 
-    if(sum <= 20 && isAlive) {
-        message = "Want to draw another card?";
-        document.getElementById("message-el").innerText = message;
-    }
-    else if(sum === 21) {
-        message = "You've got a blackjack";
-        document.getElementById("message-el").innerText = message;
-        hasBlackJack = true;
-    }
-    else {
-        isAlive = false;
-        message = "You're out of the game";
-        document.getElementById("message-el").innerText = message;
+        if(sum <= 20 && isAlive) {
+            message = "Want to draw another card?";
+            document.getElementById("message-el").innerText = message;
+        }
+        else if(sum === 21) {
+            message = "You've got a blackjack";
+            document.getElementById("message-el").innerText = message;
+            hasBlackJack = true;
+        }
+        else {
+            isAlive = false;
+            message = "You're out of the game";
+            document.getElementById("message-el").innerText = message;
+        }
     }
 }
 
 function newgame() {
     count=0;
     isAlive = true;
+    has_started = false;
     document.getElementById("cards").innerHTML = "Cards : " + `<span id="card1"></span> <span id="card2"></span> `
     document.getElementById("message-el").innerText = "";
     start();
@@ -106,6 +113,24 @@ function newcard() {
                     document.getElementById("message-el").innerText = message;
                 }
                 break;
+            }
+        case 4:
+            if(sum <= 20 && isAlive) {
+                let card6 = Math.floor(Math.random() * (max-min)) + min;
+                sum += card6;
+                document.getElementById("cards").innerHTML += ` <span id="card6"></span>`;
+                document.getElementById("card6").innerHTML = card6;
+                document.getElementById("sum").textContent = sum;
+                if(sum > 21) {
+                    isAlive = false;
+                    message = "You're out of the game";
+                    document.getElementById("message-el").innerText = message;
+                }
+                else if(sum === 21) {
+                    message = "You've got a blackjack";
+                    document.getElementById("message-el").innerText = message;
+                }
+                break;                
             }
     }
     
