f723dedee939eaf2da72c6eda442f2c29dfbecc0
nakshsharma922 <nakshsharma922@gmail.com>
2025-12-03 19:00:35 +0530

    Auto update

---
+++ python_files/whatever.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy import sparse
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+
+# x = np.linspace(-4,4,20)
+# y = np.cos(x)
+# plt.plot(x, y, marker="x")
+# plt.show()
+
+
+#          Creating data frames with pandas
+# data = {'Name': ["John","Anna","Peter","Linda"],
+#         'Location' : ["New York", "Paris", "Berlin","London"],
+#         'Age' : [24,13,53,33]
+#        }
+
+# data_pandas = pd.DataFrame(data)
+# print(data_pandas)
