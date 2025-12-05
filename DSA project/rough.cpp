ecd5a4c4daf2a6e4df2ef5a7be8dff58030b5ad8
nakshsharma922 <nakshsharma922@gmail.com>
2025-12-06 00:12:57 +0530

    Auto update

--- DSA project/rough.cpp
+++ DSA project/rough.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#include<fstream>
 using namespace std;
 
 class chotu {
@@ -20,6 +21,47 @@ public:
     }
 };
 
+class parent { 
+public : 
+    int a;
+    virtual void show() {
+        cout << "Parent class hai ye" << endl;
+    }
+};
+
+class child : public parent { 
+public:
+    int b;
+    void show() {
+        cout << "Child class hai ye" <<endl;
+    }
+};
+
+
+class top {
+    public:
+    int x;
+};
+
+class left : virtual public top {
+    public:
+    void show() {
+        cout << "In Left class" << endl;
+    }
+};
+
+class right : virtual public top {
+    public:
+    void show() {
+        cout << "In Right class" << endl;
+    }
+};
+
+class bottom : public left ,public right {
+    public:
+};
+
+
 int main() {
     // chotu gadha(1000);
     // chotu adha = gadha;
@@ -34,6 +76,24 @@ int main() {
     // cout << *(adha.x);
 
 
-    
+    //         ---runtime polymorphism---
+    // child* beta = new child();
+    // parent* papa = beta;
 
+    // papa->show();
+    // beta->show();
+
+    // bottom obj;
+    // obj.left::show();
+    // obj.right::show();
+
+    fstream file("data.txt", ios::out);
+    // if(!file.is_open()) {
+    //     cerr << "Cannot open the file for writing" << endl;
+    //     return 1;   
+    // }
+
+    file << "Hello world ye file me likh pa bhi rha hoon main ke nahi ??" << endl;
+    file << "Line 2" << endl;
+    file.close();
 }
